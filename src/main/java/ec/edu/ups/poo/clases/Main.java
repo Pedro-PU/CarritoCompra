5d083a66a46af64bf5dd96f29690c81c8d5c9bef
markzuck3rberg <141587136+markzuck3rberg@users.noreply.github.com>
2025-07-14 12:02:14 -0500

    feat: agrego comentario

--- src/main/java/ec/edu/ups/poo/clases/Main.java
+++ src/main/java/ec/edu/ups/poo/clases/Main.java
@@ -23,7 +23,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
+//Prueba 1
 public class Main {
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(() -> {
